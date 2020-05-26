8be4fab102baf32f15d3d65b7f3cbc08843d932e
parthsujalshah <parthsujalshah@gmail.com>
2020-05-26 19:19:13 +0530

    added models

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'ABC',
-    'title': 'Post 1',
-    'content':'First Post',
-    'date_posted': 'August 27, 2020'
-  },
-  {
-    'author': 'XYZ',
-    'title': 'Post 2',
-    'content':'Second Post',
-    'date_posted': 'August 28, 2020'
-  }
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 

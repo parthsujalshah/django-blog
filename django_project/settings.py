7642763a4a2eac7be45d4f1369df10ffb9262c32
parthsujalshah <parthsujalshah@gmail.com>
2021-12-04 14:14:58 +0530

    working

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('DB_NAME', 'djangoblog'),
+            'USER': os.environ.get('DB_USERNAME', 'djangoblog'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'djangoblog'),
+            'HOST': os.environ.get('DB_HOSTNAME', 'localhost'),
+            'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
 
